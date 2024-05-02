ecd6a8306795686d70eeee7df61b071e2944948f
kawodyaarchchige <kawodya.wa@gmail.com>
2024-05-02 19:41:56 +0530

    added single page application

---
+++ script.js
@@ -0,0 +1,57 @@
+$('#item-section').css({
+    display:'none'
+})
+$('#customer').css({
+    display:'none'
+})
+$('#place-order-section').css({
+    display:'none'
+})
+$('#order-details-section').css({
+    display:'none'
+})
+$('#nav-home').on('click', ()=>{
+    $('#main-section').css({
+        display:'block'
+    })
+    $('#item-section ,#customer , #place-order-section, #order-details-section').css({
+        display:'none'
+    })
+    console.log('Main-Page')
+});
+$('#nav-customer').on('click', ()=>{
+    $('#customer').css({
+        display:'block'
+    })
+    $('#item-section ,#main-section , #place-order-section, #order-details-section').css({
+        display:'none'
+    })
+    console.log('Customer-Page')
+});
+$('#nav-item').on('click', ()=>{
+    $('#item-section').css({
+        display:'block'
+    })
+    $('#customer ,#main-section, #place-order-section, #order-details-section').css({
+        display:'none'
+    })
+    console.log('Item-Page')
+});
+$('#nav-order').on('click', ()=> {
+    $('#place-order-section').css({
+        display: 'block'
+    })
+    $('#item-section ,#customer , #main-section, #order-details-section').css({
+        display: 'none'
+    })
+    console.log('Order-Page')
+});
+$('#nav-order-details').on('click', ()=>{
+    $('#order-details-section').css({
+        display:'block'
+    })
+    $('#item-section ,#customer, #main-section , #place-order-section').css({
+        display:'none'
+    })
+    console.log('Order-Details-Page')
+})
