49bd4aceb117e665549408d1a4a908c15820de8f
kawodyaarchchige <kawodya.wa@gmail.com>
2024-05-07 15:42:08 +0530

    made save cutomer & load customerTable

--- controller/customerController.js
+++ controller/customerController.js
@@ -1,53 +1,30 @@
 
-import CustomerModel  from "../model/customerModel";
-import {customers} from "../db/db";
+import CustomerModel  from "../model/customerModel.js";
+import {customers} from "../db/db.js";
 
-$(`#save-btn`).on(`click`, () => {
+$(`#btnSaveCustomer`).on('click', () => {
 
-    let cusId = $('#saveUserIdField').val();
-    let cusName = $('#saveUserNameField').val();
-    let cusEmail = $('#saveUserEmailField').val();
-    let cusAddress = $('#saveUserAddressField').val();
-    let cusBranch = $('#saveUserBranchField').val();
+    let id = $('#customerId').val();
+    let name = $('#customerFullName').val();
+    let address = $('#customerAddress').val();
+    let contact = $('#customerContact').val();
 
-    if (!cusId ||!cusName ||!cusEmail ||!cusAddress ||!cusBranch) {
-        alert('Please fill in all fields.');
-        return;
-    }
-    var customer = new CustomerModel(id,name,address,contact);
-    console.log(customer);
+    let customer = new CustomerModel(id, name, address, contact);
     customers.push(customer);
     console.log(customers);
     loadTable();
-    $(`#clear-btn`).click();
-
-});
-
+})
 function loadTable() {
-    var tbody = $(`#customer-tbody`);
-    tbody.empty();
-    customers.map((customer) => {
-
+    $(`#customer-tbl-tbody`).empty();
+    customers.map(customer => {
         var record = `<tr>
-                <td class="st-id-val">${customer.id}</td>
-                <td class="st-name-val">${customer.name}</td>
-                <td class="st-address-val">${customer.address}</td>
-                <td class="st-contact-val">${customer.branch}</td>
-            </tr>`;
+            <td>${customer.id}</td>
+            <td>${customer.name}</td>
+            <td>${customer.address}</td>
+            <td>${customer.contact}</td>
+        </tr>`;
+        $(`#customer-tbl-tbody`).append(record);
+    })
 
-        $('#st-tbody').append(record);
-    });
+
 }
- /*   customers.forEach((customer) => {
-
-
-    }*/
-       /* var tr = $(`<tr></tr>`);
-        tr.append($(`<td>${customer.id}</td>`));
-        tr.append($(`<td>${customer.name}</td>`));
-        tr.append($(`<td>${customer.address}</td>`));
-        tr.append($(`<td>${customer.contact}</td>`));
-        tr.append($(`<td><button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#update-user-modal" id="update-btn"
-onclick="update(${customer.id})">Update</button></td>`)); };
-    tbody.append(tr);*/
-
