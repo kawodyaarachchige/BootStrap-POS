701a2051dcefee81b8f40d2a92c6920dfdb9a310
kawodyaarchchige <kawodya.wa@gmail.com>
2024-05-09 00:09:24 +0530

    made a search customer funtion

--- controller/customerController.js
+++ controller/customerController.js
@@ -1,7 +1,8 @@
-
-import CustomerModel  from "../model/customerModel.js";
+import CustomerModel from "../model/customerModel.js";
 import {customers} from "../db/db.js";
-let selectedIndex ;
+
+let selectedIndex;
+let searchedCustomersArr=[];
 
 
 function loadTable() {
@@ -16,6 +17,7 @@ function loadTable() {
         $("#customer-tbl-tbody").append(record);
     })
 }
+
 $(`#btnSaveCustomer`).on('click', () => {
 
     let id = $('#customerId').val();
@@ -32,8 +34,8 @@ $(`#btnSaveCustomer`).on('click', () => {
 $('#customer-tbl-tbody').on('click', 'tr', function () {
     let selectedCusId = $(this).find("#cus-id-tbl").text();
     let selectedCusName = $(this).find("#cus-name-tbl").text();
-    let selectedCusAddress=$(this).find("#cus-address-tbl").text();
-    let selectedCusContact=$(this).find("#cus-contact-tbl").text();
+    let selectedCusAddress = $(this).find("#cus-address-tbl").text();
+    let selectedCusContact = $(this).find("#cus-contact-tbl").text();
 
 
     $('#customerId').val(selectedCusId);
@@ -41,10 +43,10 @@ $('#customer-tbl-tbody').on('click', 'tr', function () {
     $('#customerAddress').val(selectedCusAddress);
     $('#customerContact').val(selectedCusContact);
 
-    selectedIndex=$(this).index();
-    console.log("Customer Selected : ",selectedIndex);
+    selectedIndex = $(this).index();
+    console.log("Customer Selected : ", selectedIndex);
 })
-$('#btnUpdateCustomer').on('click',()=>{
+$('#btnUpdateCustomer').on('click', () => {
     let id = $('#customerId').val();
     let name = $('#customerFullName').val();
     let address = $('#customerAddress').val();
@@ -54,8 +56,10 @@ $('#btnUpdateCustomer').on('click',()=>{
     customers[selectedIndex].address = address;
     customers[selectedIndex].contact = contact;
     loadTable();
-    console.log("Customer Updated : ",customers[selectedIndex]);
-    setTimeout(()=>{alert("Customer Updated")},800);
+    console.log("Customer Updated : ", customers[selectedIndex]);
+    setTimeout(() => {
+        alert("Customer Updated")
+    }, 800);
 });
 $('#btnDeleteCustomerModal').on('click', () => {
     $('#btnDeleteCustomer').click();
@@ -68,5 +72,49 @@ $('#btnConfirmDeleteCustomer').on('click', () => {
     customers.splice(selectedIndex, 1);
     $('#cancelDeleteCustomer').click();
     loadTable();
-    setTimeout(() => {alert("Customer Deleted Successfully")},800);
+    setTimeout(() => {
+        alert("Customer Deleted Successfully")
+    }, 800);
 })
+$('#btnClearCustomer').on('click', () => {
+    loadTable();
+})
+
+$('#btnSearchCustomer').on('click', () => {
+    console.log("Search Customer Clicked");
+    try {
+        let selectedSearchingType = $('#cmbSearchByCustomer').val();
+        console.log(selectedSearchingType);
+        searchedCustomersArr.splice(0, searchedCustomersArr.length);
+        let searchText = $('#searchCustomerReference').val();
+        if(selectedSearchingType==="1"){
+            let matchedCustomer =customers.find(customer => customer.name === searchText);
+            searchedCustomersArr.push(matchedCustomer);
+        }
+        else if(selectedSearchingType==="2"){
+            let matchedCustomer =customers.find(customer => customer.id === searchText);
+            searchedCustomersArr.push(matchedCustomer);
+        }else if(selectedSearchingType==="3"){
+            let matchedCustomer =customers.find(customer => customer.address === searchText);
+            searchedCustomersArr.push(matchedCustomer);
+        }
+        console.log(searchedCustomersArr);
+        loadSearchedCustomersToTable()
+
+    }catch (error) {
+        alert("No Customer Found");
+    }
+});
+
+function loadSearchedCustomersToTable() {
+    $('#customer-tbl-tbody').empty();
+    searchedCustomersArr.map(customer => {
+        var record = `<tr>
+            <td id="cus-id-tbl">${customer.id}</td>
+            <td id="cus-name-tbl">${customer.name}</td>
+            <td id="cus-address-tbl">${customer.address}</td>
+            <td id="cus-contact-tbl">${customer.contact}</td>
+        </tr>`;
+        $("#customer-tbl-tbody").append(record);
+    })
+}
